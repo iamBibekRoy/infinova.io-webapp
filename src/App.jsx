8b91e5520c410aa66459619bfe01f00accd9a8bf
Bibek Roy <work.bibekroy@gmail.com>
2026-03-20 14:47:39 +0530

    contact us section done!!

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import TrustedCompany from "./components/TrustedCompany";
 import Services from "./components/Services";
 import OurWork from "./components/OurWork";
 import Teams from "./components/Teams";
+import ContactUs from "./components/ContactUs";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
 	const [theme, setTheme] = useState(
@@ -13,6 +15,8 @@ const App = () => {
 
 	return (
 		<div className="dark:bg-black relative">
+			<Toaster />
+
 			<Navbar
 				theme={theme}
 				setTheme={setTheme}
@@ -27,6 +31,8 @@ const App = () => {
 			<OurWork />
 
 			<Teams />
+
+			<ContactUs />
 		</div>
 	);
 };
---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import Title from "./Title";
+import { ArrowRightIcon, MailIcon, UserRoundIcon } from "lucide-react";
+import toast from "react-hot-toast";
+
+const ContactUs = () => {
+	const onSubmit = async (event) => {
+		event.preventDefault();
+
+		const formData = new FormData(event.target);
+		formData.append("access_key", "d975f57e-5521-4e6d-9167-22951ac95a0f");
+
+		try {
+			const response = await fetch("https://api.web3forms.com/submit", {
+				method: "POST",
+				body: formData,
+			});
+
+			const data = await response.json();
+			if (data.success) {
+				toast.success("Thank you for your submission!");
+				event.target.reset();
+			} else {
+				toast.error(data.message);
+			}
+		} catch (error) {
+			toast.error(error.message);
+		}
+	};
+
+	return (
+		<div
+			id="contact-us"
+			className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white">
+			<Title
+				title="Drop us Your Message"
+				desc="Let's work together"
+			/>
+
+			<form
+				onSubmit={onSubmit}
+				className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
+				<div className="">
+					<p className="mb-2 text-sm font-medium">Your Name</p>
+					<div className="flex items-center pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+						<UserRoundIcon
+							size={20}
+							className="text-gray-600"
+						/>
+						<input
+							name="name"
+							type="text"
+							placeholder="Enter your name"
+							className="w-full p-3 text-sm outline-none"
+							required
+						/>
+					</div>
+				</div>
+
+				<div className="">
+					<p className="mb-2 text-sm font-medium">Email id</p>
+					<div className="flex items-center pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+						<MailIcon
+							size={20}
+							className="text-gray-600"
+						/>
+						<input
+							name="email"
+							type="email"
+							placeholder="Enter your email"
+							className="w-full p-3 text-sm outline-none"
+							required
+						/>
+					</div>
+				</div>
+
+				<div className="sm:col-span-2">
+					<p className="mb-2 text-sm font-medium">Message</p>
+					<textarea
+						rows={8}
+						name="message"
+						placeholder="Enter your message"
+						className="w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600"
+						required
+					/>
+				</div>
+
+				<button
+					type="submit"
+					className="w-max text-sm flex items-center gap-2 bg-teal-500 hover:bg-linear-to-r hover:from-teal-600 hover:to-teal-400 text-white px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-all duration-300">
+					Submit <ArrowRightIcon size={18} />
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default ContactUs;
